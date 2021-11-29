518c92822495533dda28fa412ed28dc38bbf76aa
rsundar <rohan.s.1987@gmail.com>
2021-11-29 09:37:30 +0530

    testing makefile

--- vegam.go
+++ vegam.go
@@ -7,3 +7,14 @@ func TestFunc(a, b int) int {
 func SubtractInt(a, b int) int {
 	return a - b
 }
+
+func MultiplyInt(a, b int) int {
+	return a * b
+}
+
+func MultiplyAndAccumulate(a, b, c int) int {
+	product := a * b
+	sum := product + c
+
+	return sum
+}
